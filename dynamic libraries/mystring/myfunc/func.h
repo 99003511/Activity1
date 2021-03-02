033b023af762e98c13189729201ce5a0f5461510
Aakarsh Mehta <aakarsh.mehta@ltts.com>
2021-03-02 11:36:21 +0530

    .

---
+++ dynamic libraries/mystring/myfunc/func.h
@@ -0,0 +1,10 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+
+int prime(int n);
+int fact(int n);
+int checkpalindrome(int num);
+int add(int number ...);
+
+
+#endif // FUNC_H_INCLUDED
---
+++ dynamic libraries/mystring/mystring.h
@@ -0,0 +1,11 @@
+#ifndef STRING_H_INCLUDED
+#define STRING_H_INCLUDED
+
+
+const char *concat(char* s1, char* s2);
+int mystrlen(char* s1);
+char *mystrcpy(char* s1);
+int *mystrcmp(char* s1,char* s2);
+
+#endif 
+// STRING_H_INCLUDED
